01721c302d6433a4ba7fb2112f403845c849d02d
Daniele Campari <danielecampari@gmail.com>
2022-10-25 18:32:57 +0200

    add CRUD to Project class

--- src/index.js
+++ src/index.js
@@ -6,11 +6,28 @@ const todo2 = new Task('Play', 'Potato', new Date(2022, 9, 26), 1, null);
 const project1 = new Project('Week');
 const project2 = new Project('Today');
 
-console.log(todo1);
-console.log(todo2);
 
-console.log(project1)
-console.log(project2);
+
+
+project1.createTask(todo1);
+project1.createTask(todo2);
+// console.log(project1.data)
+// console.log(todo1);
+// console.log(todo1.id);
+// console.log(todo2);
+// console.log(todo2.id);
+// console.log(project1)
+// console.log(project2);
+
+// console.log(project1.readTask(1));
+project1.updateTask(1, 'title', 'Sleep');
+
+// console.log(project1)
+
+project1.removeTask(2);
+console.log(project1);
+
+
 
 
 
--- src/modules/classes.js
+++ src/modules/classes.js
@@ -21,4 +21,32 @@ export class Project {
         this.title = title;
         this.data = [];
     }
+
+    createTask(task) {
+        this.data.push(task);
+    }
+
+    readTask(id) {
+        let length = this.data.length;
+        for (let i = 0; i < length; i++) {
+            if ('id' in this.data[i] && this.data[i].id === id) return this.data[i];
+        }
+        return false;
+    }
+
+    updateTask(id, key, value) {
+        let length = this.data.length;
+        for (let i = 0; i < length; i++) {
+            if ('id' in this.data[i] && this.data[i].id === id && key in this.data[i]) return this.data[i][key] = value;
+        }
+        return false;
+    }
+
+    removeTask(id) {
+        let length = this.data.length;
+        for (let i = 0; i < length; i++) {
+            if ('id' in this.data[i] && this.data[i].id === id) return this.data.splice(i, 1);
+        }
+        return false;
+    }
 } 
