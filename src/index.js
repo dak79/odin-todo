fb12a5301bb757af289e5516ab8a889b81cca470
Daniele Campari <danielecampari@gmail.com>
2022-10-25 13:53:33 +0200

    add class Task

--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
-import { todo } from "./modules/factory";
+import { Task } from "./modules/classes";
 
-const todo1 = todo('Cook', 'Pasta al pesto', new Date(2022, 10, 25), 1, null);
+const todo1 = new Task('Cook', 'Pasta al pesto', new Date(), 1, null);
+const todo2 = new Task('Play', 'Potato', new Date(2022, 9, 26), 1, null);
 console.log(todo1);
+console.log(todo2);
---
+++ src/modules/classes.js
@@ -0,0 +1,17 @@
+import { format } from 'date-fns';
+export class Task {
+    static #id = 0;
+    static #incrementId() {
+        this.#id++;
+    }
+
+    constructor(title, description, dueDate, priority, checklist){
+        Task.#incrementId();
+        this.id = Task.#id;
+        this.title = title;
+        this.description = description;
+        this.dueDate = format(dueDate, 'dd-MM-yyyy');
+        this.priority = priority;
+        this.checklist = checklist;
+    }
+}
