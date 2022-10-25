b82977c4b3346ed3befe4f5578043fe2116c0ea1
Daniele Campari <danielecampari@gmail.com>
2022-10-25 20:05:49 +0200

    add comment to class.js

--- src/modules/classes.js
+++ src/modules/classes.js
@@ -1,11 +1,24 @@
 import { format } from 'date-fns';
+
+/**
+ * Create a new Task
+ * @class
+ */
 export class Task {
     static #id = 0;
     static #incrementId() {
         this.#id++;
     }
 
+    /**
+     * @param {String} title 
+     * @param {String} description 
+     * @param {Date} dueDate 
+     * @param {Number} priority 
+     * @param {Array.String} checklist 
+     */
     constructor(title, description, dueDate, priority, checklist){
+        this.type = 'task';
         Task.#incrementId();
         this.id = Task.#id;
         this.title = title;
@@ -16,36 +29,71 @@ export class Task {
     }
 }
 
+/**
+ * Create a new Project
+ * @class
+ */
 export class Project {
+
+    /**
+     * @param {String} title 
+     */
     constructor(title) {
         this.title = title;
         this.data = [];
     }
-
-    createTask(task) {
-        this.data.push(task);
+    
+    static #isTask(task) {
+        return task.type === 'task' ? true : false;
+        
     }
 
+    /**
+     * Add new task to data []
+     * @param {Object.Task} task 
+     */
+    createTask(task) {
+        Project.#isTask(task) ? this.data.push(task) : false;
+    }
+
+    /**
+     * 
+     * @param {number} id 
+     * @returns {Object.Task|false} Searched task or false if nothing found 
+     */
     readTask(id) {
         let length = this.data.length;
         for (let i = 0; i < length; i++) {
-            if ('id' in this.data[i] && this.data[i].id === id) return this.data[i];
+            if (Project.#isTask(this.data[i]) && this.data[i].id === id) return this.data[i];
         }
         return false;
     }
 
+    /**
+     * 
+     * @param {Number} id 
+     * @param {String} key 
+     * @param {String} value 
+     * @returns {Object.Task|false} Updated task or false if nothing 
+     * found 
+     */
     updateTask(id, key, value) {
         let length = this.data.length;
         for (let i = 0; i < length; i++) {
-            if ('id' in this.data[i] && this.data[i].id === id && key in this.data[i]) return this.data[i][key] = value;
+            if (Project.#isTask(this.data[i]) && this.data[i].id === id && key in this.data[i]) return this.data[i][key] = value;
         }
         return false;
     }
 
+    /**
+     * 
+     * @param {Number} id 
+     * @returns {Object.Task|false} Removed task or false if nothing found
+     */
     removeTask(id) {
         let length = this.data.length;
         for (let i = 0; i < length; i++) {
-            if ('id' in this.data[i] && this.data[i].id === id) return this.data.splice(i, 1);
+            if (Project.#isTask(this.data[i]) && this.data[i].id === id) return this.data.splice(i, 1);
         }
         return false;
     }
