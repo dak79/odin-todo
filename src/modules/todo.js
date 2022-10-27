3520083ce434b522a4dd0d3f564f2c89f5b501a4
Daniele Campari <danielecampari@gmail.com>
2022-10-27 13:08:50 +0200

    add example tasks

--- src/modules/todo.js
+++ src/modules/todo.js
@@ -1,5 +1,5 @@
 import { renderPage, renderLists } from './ui-renders';
-import { List } from './classes';
+import { Task, List } from './classes';
 import { selectNode, selectNodes } from './helpers';
 import { addNewList, addNewTask, showInbox, showToday, showThisWeek, showAnytime, showList } from './handlers';
 
@@ -9,6 +9,7 @@ export const lists = [];
 export const loadPage = () => {
     renderPage();
     defaultList();
+    exampleTasks();
 
     /* Event Listeners */
     const btnNewList = selectNode('#btn-new-list');
@@ -44,4 +45,18 @@ const addListenerLists = () => {
     const projects = selectNodes('.btn-lists');
     projects.forEach(project => project.addEventListener('click', showList))
 }
+
+const exampleTasks = () => {
+    const todoOne = new Task('Go to market', 'Buy fruit and vegetables', new Date(), 1, ['Bananas', 'Apples', 'Oranges', 'Tomatoes', 'Potatos', 'Salad']);
+    const todoTwo = new Task('Organize Meeting', 'Introduce new teammate', new Date(28, 9, 2022), 2, ['Send invitations', 'Prepare short presentation']);
+    const todoThree = new Task('Pay Bills', 'Pay electricity and gas bills', new Date(2, 10, 2022), 3, ['Electric Bill', 'Gas Bill']);
+    const todoFour = new Task('ToDo App', 'The Odin Project To Do App', new Date(20, 10, 2022), 1, ['Code Design', 'Classes', 'Ui', 'Logic', 'Set up webpack']);
+
+    todoOne.add(inbox);
+    todoTwo.add(inbox);
+    todoThree.add(inbox);
+    todoFour.add(inbox);
+}
+
+
 
