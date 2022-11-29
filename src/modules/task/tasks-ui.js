c822e20953bdb808a5eef6fcb0e60254c5f7a70f
Daniele Campari <danielecampari@gmail.com>
2022-11-29 14:21:55 +0100

    remove two function from task-ui

--- src/modules/task/tasks-ui.js
+++ src/modules/task/tasks-ui.js
@@ -142,8 +142,6 @@ export const expandTaskUi = task => {
         class: 'priority-wrapper'
     });
 
-    // const radio = checklistAndPriorityUi(task, 'radio', 'Priority:', ['low', 'medium', 'high'], 'priority', 'medium');
-    
     const radio = radioUi(task, 'radio', `Priority: `, ['low', 'medium', 'high']);
     priorityWrapper.appendChild(radio);
 
@@ -172,128 +170,6 @@ export const expandTaskUi = task => {
     return { wrapper, description: descriptionWrapper, priority: priorityWrapper, tags: tagsWrapper, checklist: checklistWrapper };
 }
 
-/**
- * Create a checkbox or a radio button.
- * @param { Object } object - Object for retriving data
- * @param { 'checkbox' | 'radio' } type - Input type. 
- * @param { 'radio'|'checklist'|'checklist-item'} name - Value to use as part of ids, classes, names, values attributes. Only for fieldset.
- * @param { string|null } arrayItem - Current item in an array. Only for fieldset. 
- * @param { number|null } index - Current array index. Only for fieldset.
- * @param { string|null } radioGroup - Value to use as part of name of radio group. Only for radio buttons.
- * @param { string } ariaLabel - Aria-label value. 
- * @returns { Node[] } Input and label nodes.
- */
- const checkboxAndRadioUi = (object, type, name, arrayItem, index, radioGroup, ariaLabel ) => {
-    const input = document.createElement('input');
-    setAttributes(input, {
-    type: `${type}`,
-    id: (name === 'checkbox') ? `${object.type}-${type}-${object.id}` : `${name}-${object.id}-${index}`,
-    class: (name === 'checkbox') ? `${object.type}s-${type}` : `${name}`,
-    name: (name === 'checkbox') ? `${object.type}-${type}-${object.id}` : (name === 'radio') ? `${radioGroup}-${object.id}` : `${name}`,
-    value: (name === 'checkbox') ? `checkbox-item-${object.id}` : (name === 'radio') ? `${arrayItem}` : `checklist-item-${object.id}-${index}`,
-    'aria-label': `${ariaLabel}`,
-    'data-number': `${object.id}`,
-    'data-type': `${name}-state`,
-    'data-btn': `${type}`
-    });
-
-    console.log(object);
-
-    const label = document.createElement('label');
-    setAttributes(label, {
-        for: (name === 'checkbox') ? `${object.type}-${type}-${object.id}` : `${name}-${object.id}-${index}`,
-        class: (name=== 'checkbox') ? `${object.type}-${type}-labels` : `${name}-labels`,
-        'data-number': `${object.id}`,
-        'data-type': `${object.type}`
-    });
-
-    // (arrayItem) ? label.textContent = arrayItem.charAt(0).toUpperCase() + arrayItem.slice(1) : (object.title) ? label.textContent = `${object.title}` : label.textContent = '';
-    (arrayItem) ? label.textContent = arrayItem : (object.title) ? label.textContent = `${object.title}` : label.textContent = '';
-    // (object.title) ? label.textContent = `${object.title}` : label.textContent = '';
-    
-    (object.complete) ? input.checked = true : input.checked = false;
-
-    return [input, label];
-}
-
-/**
- * Create checklist and priority Ui.
- * @param { Object } object - Object for retriving data.
- * @param { 'radio'|'checkbox' } type - Radio Button or Checkbox field.
- * @param { string } legendText - Legend text content.
- * @param { string[] } array - Radio names or Checklist label.
- * @param { string|null } radioGroup - Name attribute value's for Radio Buttons.
- * @param { string|null } defaultChecked - Set checked button by default or null for Radio Buttons.
- * @returns { Node } A fieldset with radio buttons or checkboxes. 
- */
-const checklistAndPriorityUi = (object, type, legendText, array, radioGroup, defaultChecked) => {
-
-    let name = (type === 'checkbox') ? 'checklist' : type;
-    
-    const fieldset = document.createElement('fieldset');
-    setAttributes(fieldset, {
-        id: `${name}-fieldset-${object.id}`,
-        class: `${name}-fieldset`
-    });
-
-    const legend = document.createElement('legend');
-    legend.textContent = legendText;
-
-    const wrapper = document.createElement('div');
-    setAttributes(wrapper, {
-        id: `${name}s-wrapper-${object.id}`,
-        class: `${name}s-wrapper`
-    });
-
-    if (array) {
-        array.forEach((item, index) => {
-            if (typeof item !== 'string') String(item);
-    
-            name = (type === 'checkbox') ? 'checklist-item' : type;
-    
-            const group = document.createElement('div');
-            setAttributes(group, {
-                id: `${name}-wrapper-${object.id}-${index}`,
-                class: `${name}-wrapper`
-            });
-
-            console.log(object.checklist)
-            const checkbox = checkboxAndRadioUi(object.checklist, type, name, item, index, radioGroup, (type === 'checkbox') ? 'Done / Not Done checklist check button' : `${item} Radio Button`);
-            
-            appendChildren(group, checkbox);
-            
-            if (type === 'checkbox') {
-                console.log(array, object)
-                const checklistBtnsWrapper = document.createElement('span');
-                setAttributes(checklistBtnsWrapper, {
-                    id: `${name}-btns-wrapper-${object.id}-${index}`,
-                    class: 'checklist-btns-wrapper'
-                });
-
-                const btnEditChecklist = btnsUi(object, 'checklist', 'edit', 'btns checklist-edit-btn svg-btns', `Edit checklist: ${object.checklist[index]}`, 'checklist', 'edit', index);
-    
-                const btnDeleteChecklist = btnsUi(object, 'checklist', 'delete', 'btns checklist-delete-btn svg-btns', `Delete checklist: ${object.checklist[index]}`, 'checklist', 'delete', index);
-                appendChildren(checklistBtnsWrapper, [btnEditChecklist, btnDeleteChecklist])
-                group.appendChild(checklistBtnsWrapper);
-            }
-        
-            
-            if (type === 'radio') {
-                if (object.priority) {
-                    if (object.priority === item) checkbox[0].checked = true;
-                } else {
-                    if (item === defaultChecked) checkbox[0].checked = true;
-                }
-            }
-
-            wrapper.appendChild(group);
-        });
-    }
-
-    appendChildren(fieldset, [legend, wrapper]);
-
-    return fieldset;
-}
 
 /**
  * Create description and tags Ui.
